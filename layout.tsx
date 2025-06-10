88dfee0ed7f9622ed1cb9b16af4faa4258d81e82
Rebwar Towfiqi <towfiqirebwar2@gmail.com>
2025-06-10 23:45:18 +0330

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,25 @@
+// app/[locale]/layout.tsx
+import { NextIntlClientProvider, useMessages } from 'next-intl'
+import { ReactNode } from 'react'
+
+import '../globals.css'
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: ReactNode
+  params: { locale: string }
+}) {
+  const messages = (await import(`../../locales/${locale}/common.json`)).default;
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  )
+}
