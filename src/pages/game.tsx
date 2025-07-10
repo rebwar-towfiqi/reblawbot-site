ece23abe449e18e69f203408c84b752a4376a120
Rebwar <rebwar.t@gmail.com>
2025-07-10 17:24:02 +0200

    Fix: auto-format game files

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,10 +1,10 @@
 // src/pages/game.tsx
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 export default function GameRedirect() {
   useEffect(() => {
-    window.location.href = "/game/index.html";
+    window.location.href = '/game/index.html';
   }, []);
 
   return <p>در حال بارگذاری بازی...</p>;
