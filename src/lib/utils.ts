16e5571ce8355558c9f9309063bb9779d0abf031
Rebwar <rebwar.t@gmail.com>
2025-07-14 01:12:19 +0330

    commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,4 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn(...classes: (string | undefined | null | false)[]) {
+  return classes.filter(Boolean).join(' ')
 }
+
