61a9d1ae6ba4f569c9a34b146fe4faa01f75e8e5
Rebwar <rebwar.t@gmail.com>
2025-07-14 18:40:19 +0330

    commit

--- src/components/buttons/TextButton.tsx
+++ src/components/buttons/TextButton.tsx
@@ -19,6 +19,13 @@ const TextButton = React.forwardRef<HTMLButtonElement, TextButtonProps>(
     },
     ref,
   ) => {
+    const variantClasses =
+      variant === 'primary'
+        ? 'text-primary-500 hover:text-primary-600 active:text-primary-700 disabled:text-primary-200'
+        : variant === 'basic'
+        ? 'text-black hover:text-gray-600 active:text-gray-800 disabled:text-gray-300'
+        : '';
+
     return (
       <button
         ref={ref}
@@ -28,16 +35,7 @@ const TextButton = React.forwardRef<HTMLButtonElement, TextButtonProps>(
           'button inline-flex items-center justify-center font-semibold',
           'focus-visible:ring-primary-500 focus:outline-none focus-visible:ring',
           'transition duration-100',
-          //#region  //*=========== Variant ===========
-          variant === 'primary' && [
-            'text-primary-500 hover:text-primary-600 active:text-primary-700',
-            'disabled:text-primary-200',
-          ],
-          variant === 'basic' && [
-            'text-black hover:text-gray-600 active:text-gray-800',
-            'disabled:text-gray-300',
-          ],
-          //#endregion  //*======== Variant ===========
+          variantClasses,
           'disabled:cursor-not-allowed disabled:brightness-105 disabled:hover:underline',
           className,
         )}
