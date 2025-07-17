acf2c18dfaa949263848e029c80fe28954fac03c
Rebwar <rebwar.t@gmail.com>
2025-07-18 00:43:59 +0330

    commit

--- next.config.js
+++ next.config.js
@@ -1,36 +1,18 @@
 // next.config.js
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import { resolve } from 'path';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // فعال‌سازی React Strict Mode
-  reactStrictMode: true,
-
-  // پیکربندی بین‌المللی‌سازی
-  i18n: {
-    locales: ['fa', 'en', 'ku'],
-    defaultLocale: 'fa'
-  },
-
-  // تعریف alias برای وارد کردن ماژول‌ها
-  webpack(config) {
-    // تعریف __filename و __dirname در مد ESM
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
-
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      // مسیر ریشه‌ی سورس
-      '@': join(__dirname, 'src'),
-      // مسیر فایل‌های ترجمه
-      '@locales': join(__dirname, 'src', 'locales'),
-      // مسیر public
-      '~': join(__dirname, 'public')
-    };
-
-    return config;
-  }
+export const reactStrictMode = true;
+export const i18n = {
+  locales: ['fa', 'en', 'ku'],
+  defaultLocale: 'fa'
 };
-
-export default nextConfig;
+export function webpack(config) {
+  config.resolve.alias = {
+    ...(config.resolve.alias || {}),
+    '@': resolve(__dirname, 'src'),
+    '@locales': resolve(__dirname, 'src', 'locales'),
+    '~': resolve(__dirname, 'public')
+  };
+  return config;
+}
