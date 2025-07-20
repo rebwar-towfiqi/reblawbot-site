419c3c8d68ab8bd1b70391a8664c3e4473679bfb
Rebwar <rebwar.t@gmail.com>
2025-07-21 00:54:05 +0330

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,25 @@
-'use client';
+
 
 import type { AppProps } from 'next/app';
-import { NextIntlClientProvider } from 'next-intl';
+import { IntlProvider } from 'next-intl';
 import { useEffect } from 'react';
 
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  // تنظیم رنگ پس‌زمینه برای کل سایت
+
   useEffect(() => {
     document.body.style.background =
       'linear-gradient(to bottom, #f0f4f8, #ffffff)';
   }, []);
 
   return (
-    <NextIntlClientProvider
-      locale={pageProps.locale} // ⬅️ مقدار locale به صورت صریح ارسال می‌شود
+    <IntlProvider
+      locale={pageProps.locale || 'fa'}
       messages={pageProps.messages || {}}
+      timeZone="Asia/Tehran"
     >
       <Component {...pageProps} />
-    </NextIntlClientProvider>
+    </IntlProvider>
   );
 }
