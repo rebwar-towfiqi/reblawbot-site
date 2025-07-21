059eaa64207d36b6ca28701612fcf98c15d58c14
Rebwar <rebwar.t@gmail.com>
2025-07-21 12:19:31 +0330

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,20 @@
+// pages/_app.tsx
+'use client';
 
- import type { AppProps } from 'next/app';
- import { useRouter } from 'next/router';
- import { NextIntlClientProvider } from 'next-intl';
- import { useEffect } from 'react';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { NextIntlClientProvider } from 'next-intl';
 
- export default function App({ Component, pageProps }: AppProps) {
-   const router = useRouter();
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
 
-   useEffect(() => {
-     document.body.style.background =
-       'linear-gradient(to bottom, #f0f4f8, #ffffff)';
-   }, []);
-return (
-   <NextIntlClientProvider
-      // صفحه‌ای که locale را از getStaticProps می‌گیرد
-      locale={pageProps.locale || router.locale || 'fa'}
-      messages={pageProps.messages || {}}
-      timeZone='Asia/Tehran'   >
+  return (
+    <NextIntlClientProvider
+      locale={router.locale}
+      messages={pageProps.messages}
+      timeZone="Asia/Tehran"
+    >
       <Component {...pageProps} />
     </NextIntlClientProvider>
- );
- }
+  );
+}
